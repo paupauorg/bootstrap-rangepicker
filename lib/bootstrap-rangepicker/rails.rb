9c196bd6cbe89d54dd87cdcfc5ed8ed8b3ed0d01
paul <stratonaut@gmail.com>
2013-04-09 15:12:40 +0300

    init

---
+++ lib/bootstrap-rangepicker/rails.rb
@@ -0,0 +1,2 @@
+require 'bootstrap-rangepicker/rails/engine'
+require 'bootstrap-rangepicker/rails/version'
---
+++ lib/bootstrap-rangepicker/rails/version.rb
@@ -0,0 +1,5 @@
+module BootstrapRangepicker
+  module Rails
+    VERSION = '0.0.1'
+  end
+end
